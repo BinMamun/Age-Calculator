5613bb7a10e85a193371ad15eb777c15aad1f81a
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-24 23:37:16 +0600

    clean up code

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@ const calcuateBtn = document.querySelector(".js-calculate");
 const userInput = document.querySelector('.js-date-input');
 const showResult = document.querySelector(".js-result");
 
-const today = dayjs();
 userInput.max = dayjs().format("YYYY-MM-D");
 
+const today = dayjs();
 calcuateBtn.addEventListener("click", () => {
   calculateAge(userInput.value);
 })
@@ -21,24 +21,24 @@ userInput.addEventListener("keydown",
 
 function calculateAge(userDate) {
 
-  const d = userDate;
+  let year = today.$y - dayjs(userDate).$y;
+  let month = today.$M - dayjs(userDate).$M;
+  let day = today.$D - dayjs(userDate).$D;
 
-  let year = today.$y - dayjs(d).$y;
-  let month = today.$M - dayjs(d).$M;
-  let day = today.$D - dayjs(d).$D;
-
-  if (year < 0) {
-    showResult.innerHTML = month;
-  } else if (month > 0) {
-    console.log(year);
+  if (month > 0) {
+    showResult.innerHTML = year;
   } else if (month === 0 && day >= 0) {
     showResult.innerHTML = year;
-  }
-  else {
+  } else {
     year--;
-    if (month <= 0)
-      if (day > 0) month = 12 + month;
-      else month = 11 - month;
+    if (month <= 0) {
+      if (day > 0) {
+        month += 12;
+      }
+      else {
+        month = 11 - month;
+      }
+    }
   }
   if (day < 0) {
     day = findDaysofMonth() + day;
@@ -52,8 +52,6 @@ function calculateAge(userDate) {
     showResult.innerHTML = `Your age is <span>${year}</span> years, <span>${month}</span> months and <span>${day}</span> days`;
   }
 }
-
-
 
 function findDaysofMonth() {
   const date = userInput.value;
