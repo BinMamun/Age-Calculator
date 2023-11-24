ef55b90aef5bb184e6be50e473be73cd29c75a5d
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-24 21:04:20 +0600

    add scripts to the project

--- scripts.js
+++ scripts.js
@@ -1,7 +1,55 @@
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
+
+const calcuateBtn = document.querySelector(".js-calculate");
+const userInput = document.querySelector('.js-date-input');
+const showResult = document.querySelector(".js-result");
+
 const today = dayjs();
-const age = dayjs("1993-11-30");
-console.log(today.$y);
-console.log(age.$y);
-console.log((today.$y - age.$y));
+userInput.max = dayjs().format("YYYY-MM-D");
+
+calcuateBtn.addEventListener("click", () => {
+  calculateAge(userInput.value);
+})
+
+function calculateAge(userDate) {
+
+  const userData = {
+    day: 0,
+    month: 0,
+    year: 0
+  };
+
+  const d = userDate;
+  userData.day = dayjs(d).$D;
+  userData.month = dayjs(d).$M;
+  userData.year = dayjs(d).$y;
+
+  let day = today.$D - userData.day;
+  let month = (today.$M - userData.month);
+  let year = today.$y - userData.year;
+
+  if (today.$D <= userData.day) {
+    month--;
+    day += findDaysofMonth();
+  }
+  if (today.$M <= userData.month) {
+    year--;
+    month += 12;
+  }
+  if (today.$y > userData.year) {
+    showResult.innerHTML = "Invalid Date";
+  }
+
+  showResult.innerHTML = `Your age is ${year} years, ${month} months and ${day} days`
+}
+
+
+function findDaysofMonth() {
+  const date = userInput.value;
+  return dayjs(date).daysInMonth();
+}
+
+
+
+
